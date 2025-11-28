42bfe32b31da20953c7a49059462178e12411cbc
20149573 <20149573@tafe.wa.edu.au>
2025-11-28 11:24:41 +0800

    Added log cars entering and leaving

--- src/car_park.py
+++ src/car_park.py
@@ -1,14 +1,17 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime  # we'll use this to timestamp entries
 class CarPark:
-    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None):
+    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
 
         self.plates = plates or []
         self.displays = displays or []
 
-
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
     def register(self, component):
         if not isinstance(component, (Sensor, Display)):
             raise TypeError("Object must be a Sensor or Display")
@@ -23,6 +26,7 @@ class CarPark:
         if self.available_bays > 0:
             self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         if plate not in self.plates:
@@ -38,6 +42,12 @@ class CarPark:
     @property
     def available_bays(self):
         return max(self.capacity - len(self.plates), 0)
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
+
 
 
     def update_display(self, display):
